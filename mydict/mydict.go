b1c3970baf68a213e92e0ca934bf5df90463877b
GulliverNam <gullivernam94@gmail.com>
2023-02-10 05:45:20 +0000

    02 Dictionary part is done

---
+++ mydict/mydict.go
@@ -0,0 +1,61 @@
+package mydict
+
+import (
+	"errors"
+)
+
+// Dictionary type
+type Dictionary map[string]string
+
+var errNotFound = errors.New("Word Not Found")
+var errCantUpdate = errors.New("Cant Update non-existing word")
+var errCantDelete = errors.New("Cant Delete non-existing word")
+var errWordExists = errors.New("That word already exists")
+
+// Search for a word
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+	if exists {
+		return value, nil
+	}
+	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+// Delete a word
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		delete(d, word)
+	case errNotFound:
+		return errCantDelete
+	}
+	return nil
+}
+
+// func (d Dictionary) String() string {
+// 	return fmt.Sprintf("%v", d)
+// }
